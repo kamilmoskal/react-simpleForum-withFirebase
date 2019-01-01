4527d35d0a52732136567534559c133f3e0c8d14
kamilmoskal <kamil_moskal@interia.pl>
2019-01-01 12:50:48 +0100

    Updates

---
+++ precache-manifest.68231b68784621f7a0e7e7541c6b374c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5eff8a2717ebd35f7a8e",
+    "url": "/react-simpleForum-withFirebase/static/js/runtime~main.5eff8a27.js"
+  },
+  {
+    "revision": "db92406db080604503f5",
+    "url": "/react-simpleForum-withFirebase/static/js/main.db92406d.chunk.js"
+  },
+  {
+    "revision": "4b590f971e3a1216a987",
+    "url": "/react-simpleForum-withFirebase/static/js/1.4b590f97.chunk.js"
+  },
+  {
+    "revision": "db92406db080604503f5",
+    "url": "/react-simpleForum-withFirebase/static/css/main.91bcca0d.chunk.css"
+  },
+  {
+    "revision": "4b590f971e3a1216a987",
+    "url": "/react-simpleForum-withFirebase/static/css/1.eaf74bf7.chunk.css"
+  },
+  {
+    "revision": "86d53dff6542dce59da0f37324e28701",
+    "url": "/react-simpleForum-withFirebase/index.html"
+  }
+];
